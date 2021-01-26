c09189f8618687b473295ce0800c0a1f8b27b2aa
Kenneth Spry <kenneth.spry1@gmail.com>
2021-01-26 12:16:47 -0500

    no link

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import storyboard from "./assets/preemptive-alerts-storyboard.png";
 import mockup1 from "./assets/alert-fatigue-mockup-1.png";
 
